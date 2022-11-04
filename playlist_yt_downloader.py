69e6c87aeb6bd33bf8fc20d634d85599d8f98f59
Matheus Souza <matheus3864@gmail.com>
2022-11-04 17:47:44 -0300

    Screen creation

--- playlist_yt_downloader.py
+++ playlist_yt_downloader.py
@@ -1,22 +1,54 @@
 from pytube import Playlist
 from moviepy.editor import VideoFileClip
+from tkinter import *
+import os
 import glob
 
-link = Playlist(input('Insira o URL: '))
-
-def downloader(pl):
+def downloader():
+    pl = Playlist(entry.get())
     for video in pl.videos:
         ori_video = video.streams.get_highest_resolution()
-        ori_video.download()
-        titulo = video.title
-        print(f'Download feito do vídeo: {titulo}')
-    return titulo
-
-def convert(titulo):
-    for file in glob.glob("*.mp4"):
+        ori_video.download('downloads')
+        title = video.title
+        # print(f'Video downloaded: {title}')
+    print("Downloaded!")
+    convert()
+    
+def convert():
+    mp3_dir = 'C:/Users/supor/OneDrive/Documentos/.mp3'
+    for file in glob.glob("downloads/*.mp4"):
         print(file)
         videoclip = VideoFileClip(file)
         audioclip = videoclip.audio
         audioclip.write_audiofile(file[:-4]+'.mp3')
+        for file2 in os.listdir('downloads'):
+            if '.mp3' in file2:
+                os.rename(f"downloads/{file2}", mp3_dir + "/" + file2)
+    print("Converted!")
+
+def reencaminhar():
+    mp4_dir = 'C:/Users/supor/OneDrive/Documentos/.mp4'
         
-convert(downloader(link))
+    for file in os.listdir('downloads'):
+        if '.mp4' in file:
+            os.rename(f"downloads/{file}", mp4_dir + "/" + file)
+    print('Files transfered!')
+    
+window = Tk()
+window.geometry('500x180')
+window.title('YT Downloader')
+window.configure(background='#dde')
+
+text = Label(window, text="Insert the URL: ")
+text.place(x=210, y=10)
+
+entry = Entry(window, width=60)
+entry.place(x=80,y=50)
+
+download = Button(window, text="Download", command=downloader)
+download.place(x=220,y=90)
+
+foward = Button(window, text="Foward", command=reencaminhar)
+foward.place(x=227,y=130)
+
+window.mainloop()
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,9 +1,10 @@
 from pytube import YouTube
+from playlist_yt_downloader import *
 
-link = input('Insira o URL: ')
+link = input('Insert the URL: ')
 
 yt = YouTube(link)
 
-print(f"Título: {yt.title}")
+res_yt = yt.streams.get_highest_resolution().download('single videos')
 
-res_yt = yt.streams.get_highest_resolution().download()
+print(f"Downloaded video: {yt.title}")
